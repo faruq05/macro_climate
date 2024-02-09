475fd14b75cfacaaa0c132ab63c1f99b8e97d8d9
Omor Faruq <faruq05.fiverr@gmail.com>
2024-02-09 15:41:53 +0600

    Complete

--- js/script.js
+++ js/script.js
@@ -1,18 +1,69 @@
-// nav scroll snap transition
+// nav active class
 document.addEventListener("DOMContentLoaded", function () {
-    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    const navItems = document.querySelectorAll('.navbar-nav .nav-item');
 
-    navLinks.forEach(function (link) {
-        link.addEventListener('click', function (event) {
-            event.preventDefault(); // Prevent default anchor behavior
 
-            const targetId = this.getAttribute('href'); // Get target section id
-            const targetSection = document.querySelector(targetId); // Get target section element
+    navItems.forEach(function (navItem) {
 
-            if (targetSection) {
-                // Scroll to the target section with smooth behavior
-                targetSection.scrollIntoView({ behavior: 'smooth' });
-            }
+        navItem.addEventListener('click', function () {
+
+            navItems.forEach(function (item) {
+                item.classList.remove('active');
+            });
+
+
+            this.classList.add('active');
         });
     });
 });
+
+//header color
+window.addEventListener('scroll', function () {
+    // Get the current scroll position
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+    // Get the target div element
+    var targetDiv = document.getElementById('header');
+
+    // Check if scrolling down
+    if (scrollPosition > 30) {
+        // Add the class for the new background color
+        targetDiv.classList.add('scroll-down-bg');
+        targetDiv.classList.add('nl');
+    } else {
+        // Remove the class for the new background color
+        targetDiv.classList.remove('scroll-down-bg');
+        targetDiv.classList.remove('nl');
+    }
+});
+
+// logo shrink
+$(document).ready(function () {
+    $(document).scroll(function () {
+        if ($(document).scrollTop() >= 30) {
+            $('.navbar-brand img').addClass('js-scrolling');
+        } else {
+            $('.navbar-brand img').removeClass('js-scrolling');
+        }
+    });
+});
+
+
+// back to top
+window.onscroll = () => {
+    toggleTopButton();
+}
+function scrollToTop() {
+    const heroSection = document.getElementById('hero');
+    heroSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+function toggleTopButton() {
+    if (document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20) {
+        document.getElementById('back-to-up').classList.remove('d-none');
+    } else {
+        document.getElementById('back-to-up').classList.add('d-none');
+    }
+}
